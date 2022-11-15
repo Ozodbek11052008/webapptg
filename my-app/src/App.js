d0bfe90a22d4e940f24a8862007a65597657c8be
Ozodbek11052008 <ozodabduvoitov8@gmail.com>
2022-11-15 20:22:08 +0500

    Added my project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 const tg = window.Telegram.WebApp;
-
+import { React, useEffect } from "react";
 function App() {
-  
+
   useEffect(() => {
     tg.ready()
   }, [])
-  
 
-  const onClose = () => {
+  const onclose = () => {
     tg.close()
   }
-
   return (
     <>
       work
-      <button onClick={onClose}>Close</button>
+      <button>Close</button>
     </>
   );
 }
